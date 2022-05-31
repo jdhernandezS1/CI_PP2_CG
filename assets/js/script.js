00e024b2b79304248b6cacc8defe31e8ce08074c
David <julio-david@hotmail.com>
2022-05-31 20:46:23 +0200

    js_mod1.0

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// max right axis of the game div
+var rightaxis = 195;
+// max left axis of the game div
+var leftaxis = 45; 
+var vel= 0;//
+var score=0;//score variable
+var roadY=0;
+var difaxis=rightaxis-leftaxis;
+var flag = true;
+var enemy_lvl=0.9;
+var accel= 30;
+// document items references
+var container;
+var controls;
+var car;
+var notification_bar;
+var game;
+var arrow;
+// end of document items

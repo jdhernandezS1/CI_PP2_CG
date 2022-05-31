8098bfb85c6633ab56d459d79b4d3ec321a6f7ef
David <julio-david@hotmail.com>
2022-05-31 20:49:37 +0200

    js_mod1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+// initial conditions
+var t = new Date();
+var dt = 0;
+if(document.readyState === "complete" || document.readyState === "interactive"){
+    setTimeout(Init,1);
+}
+else{
+    document.addEventListener("DOMContentLoaded",Init);
+}
+// seting up the initial constructure
+function Init(){
+    t = new Date();
+    Run();
+    Bucle();
+}
+// create the bucle infinite
+function Bucle(){
+    dt = (new Date()-t )/1000;
+    t= new Date();
+    Refresh();
+    requestAnimationFrame(Bucle);
+}
+
 // max right axis of the game div
 var rightaxis = 195;
 // max left axis of the game div
@@ -17,3 +40,36 @@ var notification_bar;
 var game;
 var arrow;
 // end of document items
+
+function Run(){
+    container= document.querySelector(".container");
+    controls= document.querySelector(".controls");
+    game= document.querySelector(".game");
+    // constructor car
+    car= document.querySelector("#car");
+    car.style.left = (40)+"px";
+    // car.style.top = (350)+"px"; 
+    notification_bar= document.querySelector(".notification_bar");
+    arrow= document.querySelector(".arrow");
+}
+function Refresh(){
+
+
+}
+
+let generateEnemies = setInterval(generateCars,500);
+
+function generateCars(){
+    if(flag==true){
+
+        if(Math.random() > enemy_lvl){
+            const enemiesCars = document.createElement("div");
+            container.appendChild(enemiesCars);
+            enemiesCars.classList.add("enemies")
+            if(Math.random() > 0.5) enemiesCars.classList.add("enemies1");
+            else enemiesCars.classList.add("enemies2");
+            enemiesCars.style.left = (leftaxis+(difaxis*Math.random()))+"px";   
+            enemiesCars.style.top = -38 + "px";
+        }
+    }
+}

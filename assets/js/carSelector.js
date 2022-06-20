3d13ec3c0b449ac16b5284a3b4575e0934d7af36
David <julio-david@hotmail.com>
2022-06-20 15:45:46 +0200

    2.9 local storage parameters

--- assets/js/carSelector.js
+++ assets/js/carSelector.js
@@ -1,17 +1,41 @@
+// set default car for errors
 localStorage.setItem("storageName","car1");
-
+/** 
+   *modifi local storage variable to bring in the game
+   *Conect global variables by DOOM
+   * @param {nothing}  
+   * @returns {nothing}
+   */
 function firstCar(){
     var car = "car1";
     localStorage.setItem("storageName",car);
 }
+/** 
+   *modifi local storage variable to bring in the game
+   *Conect global variables by DOOM
+   * @param {nothing}  
+   * @returns {nothing}
+   */
 function secondCar(){
     var car = "car2";
     localStorage.setItem("storageName",car);
 }
+/** 
+   *modifi local storage variable to bring in the game
+   *Conect global variables by DOOM
+   * @param {nothing}  
+   * @returns {nothing}
+   */
 function thirdCar(){
     var car = "car3";
     localStorage.setItem("storageName",car);
 }
+/** 
+   *modifi local storage variable to bring in the game
+   *Conect global variables by DOOM
+   * @param {nothing}  
+   * @returns {nothing}
+   */
 function forthCar(){
     var car = "car4";
     localStorage.setItem("storageName",car);

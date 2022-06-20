91d9352c7cab22fa5825da800b169607fb081cc8
David <julio-david@hotmail.com>
2022-06-20 08:44:47 +0200

    2.6 contact_us Test default emailjs

--- assets/js/contact_us.js
+++ assets/js/contact_us.js
@@ -1,14 +1,16 @@
 (function() {
     // https://dashboard.emailjs.com/admin/account
-    emailjs.init('YOUR_PUBLIC_KEY');
+    emailjs.init('Y3MAEbI4jwmweVJZ');
 })();
 window.onload = function() {
+    var templateId= 'template_gen1qqe' ;
+    var serviceId='service_eli2pfr';
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('service_eli2pfr', 'template_gen1qqe', this)
+        emailjs.sendForm(serviceId,templateId, this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {

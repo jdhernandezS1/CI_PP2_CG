51a1487e03f846e32a293712a150c095611c18ce
David <julio-david@hotmail.com>
2022-06-20 09:26:55 +0200

    2.7 contact_us.js Email js Done

--- assets/js/contact_us.js
+++ assets/js/contact_us.js
@@ -1,11 +1,15 @@
 
+/**
+ * Sends contact form data to email(developer) by submiting 
+ * Code written by the help of the official EmailJS tuttorial https://www.emailjs.com/docs/tutorial/creating-contact-form/
+ */
 window.onload = function() {
     var templateId= "template_crazy_cars" ;
     var serviceId="service_crazy_cars";
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
+        // this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm(serviceId,templateId, this)
             .then(function() {

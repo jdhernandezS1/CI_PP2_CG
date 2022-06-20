b91b8e53e6dbf668cac4ee734eddcae48c7d8969
David <julio-david@hotmail.com>
2022-06-20 15:29:58 +0200

    2.9 Car selectorfunctions

---
+++ assets/js/carSelector.js
@@ -0,0 +1,18 @@
+localStorage.setItem("storageName","car1");
+
+function firstCar(){
+    var car = "car1";
+    localStorage.setItem("storageName",car);
+}
+function secondCar(){
+    var car = "car2";
+    localStorage.setItem("storageName",car);
+}
+function thirdCar(){
+    var car = "car3";
+    localStorage.setItem("storageName",car);
+}
+function forthCar(){
+    var car = "car4";
+    localStorage.setItem("storageName",car);
+}

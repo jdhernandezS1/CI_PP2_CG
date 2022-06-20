99dab1382c2b9db93dbd8fc6d104a86831cdb592
David <julio-david@hotmail.com>
2022-06-20 11:57:25 +0200

    2.7 Form js algorithm

--- assets/js/contact_us.js
+++ assets/js/contact_us.js
@@ -3,18 +3,20 @@
  * Sends contact form data to email(developer) by submiting 
  * Code written by the help of the official EmailJS tuttorial https://www.emailjs.com/docs/tutorial/creating-contact-form/
  */
-window.onload = function() {
-    var templateId= "template_crazy_cars" ;
-    var serviceId="service_crazy_cars";
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('-Y3MAEbI4jwmweVJZ');
+    var templateId = "template_crazy_cars";
+    var serviceId = "service_crazy_cars";
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         // this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm(serviceId,templateId, this)
-            .then(function() {
+        emailjs.sendForm(serviceId, templateId, this)
+            .then(function () {
                 console.log('SUCCESS!');
-            }, function(error) {
+            }, function (error) {
                 console.log('FAILED...', error);
             });
     });

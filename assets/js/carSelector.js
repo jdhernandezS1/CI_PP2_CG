aa76d2457387a56bcfd36e2a4e57464ba6fc2a38
David <julio-david@hotmail.com>
2022-06-21 21:22:51 +0200

    3.1 formating document

--- assets/js/carSelector.js
+++ assets/js/carSelector.js
@@ -1,72 +1,58 @@
 // set default car for errors
-localStorage.setItem("storageName","car1");
+localStorage.setItem("storageName", "car1");
 /** 
    *modifi local storage variable to bring in the game
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function firstCar(){
+function firstCar() {
     var car = "car1";
-    localStorage.setItem("storageName",car);
+    localStorage.setItem("storageName", car);
 }
 /** 
    *modifi local storage variable to bring in the game
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function secondCar(){
+function secondCar() {
     var car = "car2";
-    localStorage.setItem("storageName",car);
+    localStorage.setItem("storageName", car);
 }
 /** 
    *modifi local storage variable to bring in the game
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function thirdCar(){
+function thirdCar() {
     var car = "car3";
-    localStorage.setItem("storageName",car);
+    localStorage.setItem("storageName", car);
 }
 /** 
    *modifi local storage variable to bring in the game
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function forthCar(){
+function forthCar() {
     var car = "car4";
-    localStorage.setItem("storageName",car);
+    localStorage.setItem("storageName", car);
 }
 /** 
    *Set the local storage to be use by the difficult
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function easy(){
+function easy() {
 
-    localStorage.setItem("storageLvl",1);
+    localStorage.setItem("storageLvl", 1);
 }
 /** 
    *Set the local storage to be use by the difficult
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function mid(){
+function mid() {
 
-    localStorage.setItem("storageLvl",2);
+    localStorage.setItem("storageLvl", 2);
 }
 /** 
    *Set the local storage to be use by the difficult
    *Conect global variables by DOOM
-   * @param {nothing}  
-   * @returns {nothing}
    */
-function hard(){
+function hard() {
 
-    localStorage.setItem("storageLvl",3);
+    localStorage.setItem("storageLvl", 3);
 }
